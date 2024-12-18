0cd3788108e8f0fa2e86163c1839e4450bb1cdcc
sv_du <52849920+sv-du@users.noreply.github.com>
2024-12-17 20:04:51 -0800

    Upload

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Net.Http;
+using System.IO;
+using System.Diagnostics;
+
+class Program {
+    public static void Main(string[] args) {
+        if(args.Length == 0) {
+            Console.WriteLine("No URL provided");
+            return;
+        }
+        HttpClient client = new HttpClient();
+        string url = args[0];
+        string body;
+        try {
+            HttpResponseMessage res = client.GetAsync(url).Result;
+            body = res.Content.ReadAsStringAsync().Result;
+        } catch(Exception e) {
+            Console.WriteLine($"Error while fetching content: {e.Message}");
+            return;
+        }
+        StreamWriter sw = new StreamWriter($"{System.IO.Path.GetTempPath()}\\BatchFileToRun.bat");
+        sw.WriteLine(body);
+        sw.Close();
+        ExecuteCommand($"{System.IO.Path.GetTempPath()}\\BatchFileToRun.bat");
+    }
+
+    public static void ExecuteCommand(string command) {
+        ProcessStartInfo processInfo = new ProcessStartInfo("cmd.exe", $"/c \"{command}\"");
+        processInfo.CreateNoWindow = true;
+        processInfo.UseShellExecute = true;
+        Process process = Process.Start(processInfo);
+        process.WaitForExit();
+    }
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+# Download Batch File
+
+A simple tool that replicates Powershell's way of doing ``irm <link> | iex``
